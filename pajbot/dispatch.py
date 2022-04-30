91c5d32294a36531201a497de356695a35f30fee
mmattbtw <30363562+mmattbtw@users.noreply.github.com>
2022-04-30 10:30:40 -0500

    Revert "finished"
    This reverts commit 6e3c4fddbd04cfdebc4c7e8c3c5ac754bdf16317.

--- pajbot/dispatch.py
+++ pajbot/dispatch.py
@@ -52,9 +52,6 @@ class Dispatch:
         elif response.startswith("/me") or response.startswith(".me"):
             type = "me"
             response = " ".join(response.split(" ")[1:])
-        elif response.startswith("/announce") or response.startswith(".announce"):
-            type = "announcement"
-            response = " ".join(response.split(" ")[1:])
         elif options["whisper"] is False or options["reply"] is False:
             type = "say"
         action = {"type": type, "message": response}
@@ -104,9 +101,6 @@ class Dispatch:
                 type = "reply"
             elif response.startswith("/me") or response.startswith(".me"):
                 type = "me"
-                response = " ".join(response.split(" ")[1:])
-            elif response.startswith("/announce") or response.startswith(".announce"):
-                type = "announcement"
                 response = " ".join(response.split(" ")[1:])
             elif options["whisper"] is False or options["reply"] is False:
                 type = "say"
